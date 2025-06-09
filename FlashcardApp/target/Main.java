92467df6f6f5d4a68f2884b8a580ddc323433b66
daytonroberts <daytonroberts03@gmail.com>
2025-06-09 16:22:55 -0700

    now

---
+++ FlashcardApp/target/Main.java
@@ -0,0 +1,9 @@
+package com.flashcards;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(FlashcardApp.class, args);
+    }
+}
